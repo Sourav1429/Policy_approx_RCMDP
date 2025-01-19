8d244a3b465e17e840679e121c7e7fe8d6079275
Sourav1429 <68081019+Sourav1429@users.noreply.github.com>
2025-01-19 15:49:13 -0500

    Add files via upload

--- KL_uncertainity_evaluator.py
+++ KL_uncertainity_evaluator.py
@@ -6,6 +6,7 @@ Created on Sat Jan 18 14:00:27 2025
 """
 
 import numpy as np
+import torch
 
 class Robust_pol_Kl_uncertainity:
     def __init__(self,nS,nA,cost_list,init_dist):
@@ -16,28 +17,29 @@ class Robust_pol_Kl_uncertainity:
         self.gamma = 0.995
     def calculate_infinite_Q(self,n,policy,P,C_KL):
         C = self.cost_list[n]
-        Q = np.zeros(self.nS,self.nA)
-        V = np.zeros(self.nS)
+        Q = torch.zeros((self.nS,self.nA),requires_grad=True)
+        V = torch.zeros(self.nS,requires_grad=True)
         tau = 1000
         s = np.random.choice(self.nS,p=self.init_dist)
         for t in range(tau):
             a = np.random.choice(self.nA,p = policy[s])
             next_state = np.random.choice(self.nS,p=P[a,s,:])
-            P_star = np.aray([P[a,s,i]*np.exp(V[i]/C_KL) for i in range(self.nS)])
-            Q[s,a] = C[s,a] + self.gamma * np.dot(P_star,V)
-            V = np.array([np.dot(policy[s],Q[s,a]) for s in range(self.nS)])
+            P_star = torch.tensor([P[a,s,i]*np.exp(V[i]/C_KL) for i in range(self.nS)],requires_grad = True)
+            Q[s,a] = C[s,a] + self.gamma * torch.mm(P_star,V)
+            V = torch.tensor([torch.dot(policy[s],Q[s,a]) for s in range(self.nS)],requires_grad=True)
             s = next_state
         return Q,V
     def evaluate_policy(self,policy,P,C_KL,n):
         Q,V = self.calculate_infinite_Q(n, policy, P, C_KL)
-        P_star = np.zeros((self.nS,self.nA,self.nS))
-        Pi_pi = np.zeros((self.nS,self.nS,self.nA))
+        P_star = torch.zeros((self.nS,self.nA,self.nS))
+        #Pi_pi = torch.zeros((self.nS,self.nS,self.nA))
+        Q_ = torch.zeros((self.nS,self.nA))
         for s in range(self.nS):
             for a in range(self.nA):
-                P_star[s,a,:] = np.array([P(a,s,i)*np.exp(V[i]/C_KL) for i in range(self.nS)])
-            Pi_pi[s,s,:] = policy[s,:] 
-        inv = np.linalg.inv(np.eye(self.nS) - self.gamma*np.matmul(P_star,Pi_pi))
-        Q_ = np.matmul(inv,self.cost_list[n])
-        J = np.sum([self.init_dist[s]*np.sum([policy[s,a]*Q_[s,a] for a in range(self.nA)]) for s in range(self.nS)])
+                P_star[s,a,:] = torch.tensor([P(a,s,i)*np.exp(V[i]/C_KL) for i in range(self.nS)],requires_grad=True)
+                Q_[s,a] = self.cost_list[n][s,a] + self.gamma*torch.sum([P_star[s,a,s_next]*torch.sum([policy[s_next,a_next]*Q_[s_next,a_next] for a_next in range(self.nA)]) for s_next in range(self.nS)])#not correct
+        #inv = np.linalg.inv(np.eye(self.nS) - self.gamma*np.matmul(P_star,Pi_pi))
+        #Q_ = np.matmul(inv,self.cost_list[n])
+        J = torch.sum([self.init_dist[s]*torch.sum([policy[s,a]*Q_[s,a] for a in range(self.nA)]) for s in range(self.nS)])
         return J
         
--- Projected_RCMDP.py
+++ Projected_RCMDP.py
@@ -8,7 +8,14 @@ Created on Sat Jan 18 13:53:04 2025
 import numpy as np
 from Machine_Rep import Machine_Replacement
 from KL_uncertainity_evaluator import Robust_pol_Kl_uncertainity
+import torch
+import pickle
 
+def Proj(policy,V,Pi):
+    policy = policy + torch.gradient(V)
+    smallest_distance = np.argmin([np.norm(policy-pi) for pi in Pi])
+    return Pi[smallest_distance]
+    
 mr_obj = Machine_Replacement()
 ch = 2
 P,R,C = mr_obj.gen_probability(),mr_obj.gen_expected_reward(ch),mr_obj.gen_expected_cost()
@@ -18,16 +25,30 @@ init_dist = np.array([0.8,0.04,0.05,0.11])
 pol_eval = Robust_pol_Kl_uncertainity(nS, nA, cost_list, init_dist)
 C_KL = 0.5 # what will be this parameter
 store=[]
+eps = 0.01
+b = 3
 
 policy = np.zeros((nS,nA))*1/nA
+policy_space= []
+n_pol = np.power(nA,nS)
+vf_store = []
+for i in range(n_pol):
+    policy_space.append(list(map(int,format(i, '04b'))))
+policy_space = np.array(policy_space)
 T = 1000
 for t in range(T):
     Vr,Vc = pol_eval.evaluate_policy(policy, P, C_KL, 0),pol_eval.evaluate_policy(policy, P, C_KL, 1)
-    if(Vr >= b - eps):
-        policy = Proj(policy,Vr) ##define this
+    if(Vc >= b - eps):
+        policy = Proj(policy,Vr,policy_space) ##define this
     else:
-        policy = Proj(policy,Vc)
+        policy = Proj(policy,Vc,policy_space)
     store.append(np.min(Vr,-np.clip((b-Vc),0,np.inf)))
-with open("Store_robust_vf","wb") as f:
+    vf_store.append(Vr)
+print(np.argmax(store))
+with open("Store_robust_output","wb") as f:
     pickle.dump(store,f)
+f.close()
+
+with open("Store_robust_vf","wb") as f:
+    pickle.dump(vf_store,f)
 f.close()
